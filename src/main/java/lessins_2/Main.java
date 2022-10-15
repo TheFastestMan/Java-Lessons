ee64b787a5db9f4677a8dbc146ec98e771eaf4a9
TheFastestMan <Rail_ravilov@bk.ru>
2022-10-15 16:03:58 +0500

    Запятые, пробелы, статический метод

--- src/main/java/lessins_2/Main.java
+++ src/main/java/lessins_2/Main.java
@@ -3,16 +3,18 @@ import java.util.EnumMap;
 import java.util.Map;
 import java.util.Set;
 
+import static lessins_2.Animal.*;
+
 public class Main {
     public static void main(String[] args) {
         Map<Animal, String> enumMap = new EnumMap<Animal, String>(Animal.class);
 
-        enumMap.put(Animal.HORSE,null); // We can have a null as a value but cannot as a key
-        enumMap.put(Animal.SPIDER,"Two");
-        enumMap.put(Animal.SCORPION,"Three");
-        enumMap.put(Animal.FISH,"Four");
-        enumMap.put(Animal.DOG,"Five");
-        enumMap.put(Animal.CAT,"Six");
+        enumMap.put(HORSE, null); // We can have a null as a value but cannot as a key
+        enumMap.put(SPIDER, "Two");
+        enumMap.put(SCORPION, "Three");
+        enumMap.put(FISH, "Four");
+        enumMap.put(DOG, "Five");
+        enumMap.put(CAT, "Six");
 
         Set <Map.Entry<Animal, String>> entrySet = enumMap.entrySet();
 
