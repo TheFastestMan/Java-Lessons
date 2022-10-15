7be69f48d54778f331043654076130924a3e6f7f
TheFastestMan <Rail_ravilov@bk.ru>
2022-10-15 11:09:59 +0500

    Initial commit

--- src/main/java/lessins_2/Main.java
+++ src/main/java/lessins_2/Main.java
@@ -1,7 +1,27 @@
 package lessins_2;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
+        Map<Animal, String> enumMap = new EnumMap<Animal, String>(Animal.class);
+
+        enumMap.put(Animal.HORSE,"One");
+        enumMap.put(Animal.SPIDER,"Two");
+        enumMap.put(Animal.SCORPION,"Three");
+        enumMap.put(Animal.FISH,"Four");
+
+        Set set = enumMap.entrySet();
+
+        for (Object o : set) {
+            Map.Entry m = (Map.Entry)o;
+            System.out.println(m.getKey()+" "+m.getValue());
+        }
+
+
+        System.out.println(enumMap);
+
 
     }
 }
