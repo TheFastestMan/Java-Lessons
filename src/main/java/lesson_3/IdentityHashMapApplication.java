cd279fa8de6921c81f923183adfcc077c5761bc7
TheFastestMan <Rail_ravilov@bk.ru>
2022-10-15 12:54:23 +0500

    Added just HashMap and compared the differences

--- src/main/java/lesson_3/IdentityHashMapApplication.java
+++ src/main/java/lesson_3/IdentityHashMapApplication.java
@@ -1,5 +1,6 @@
 package lesson_3;
 
+import java.util.HashMap;
 import java.util.IdentityHashMap;
 import java.util.Map;
 import java.util.Set;
@@ -18,8 +19,25 @@ public class IdentityHashMapApplication {
                 Set m = ihm.entrySet();
 
                 for (Object o : m) {
-                        Map.Entry s = (Map.Entry) o ;
+                        Map.Entry <Integer,String> s = (Map.Entry<Integer,String>) o ;
                         System.out.println(s.getKey()+ "  "+s.getValue());
+                }
+                System.out.println();
+
+                HashMap<Integer, String> hm = new HashMap();
+                hm.put(new Integer(1),"One");
+                hm.put(new Integer(1),"One");
+                hm.put(new Integer(2),"Two");
+                hm.put(new Integer(2),"Two");
+                hm.put(new Integer(21),"Twenty One");
+
+                System.out.println(hm);
+
+                Set q = hm.entrySet();
+
+                for (Object p : q) {
+                        Map.Entry r = (Map.Entry) p ;
+                        System.out.println(r.getKey()+ "  "+r.getValue());
                 }
 
 
